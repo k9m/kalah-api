bff781cef21d5f4154705bf98fc167a4743aeb3a
k9m <1000999m@gmail.com>
2020-03-30 01:11:37 +0200

    Security

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 APP_JAR=$(ls *.jar)
-command="java -jar $APP_JAR --spring.profiles.active=docker"
+command="java -jar $APP_JAR --spring.profiles.active=docker --spring.security.ouath2.client.secret=$KEYCLOAK_SECRET"
 echo $command
 exec $command
--- src/main/java/org/k9m/kalah/KalahApiApplication.java
+++ src/main/java/org/k9m/kalah/KalahApiApplication.java
@@ -1,10 +1,12 @@
 package org.k9m.kalah;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
 @EnableMongoRepositories(basePackages = "org.k9m.kalah.persistence")
 public class KalahApiApplication {
 
